5a53b622a6e60d76efa0428061bdaa524e208766
ZetInMark <mark.czetin@gmail.com>
2023-10-19 17:20:23 +0200

    movement_and_animation_made

--- Assets/Scripts/playerScripts/playerMovement.cs
+++ Assets/Scripts/playerScripts/playerMovement.cs
@@ -7,35 +7,58 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Speed at which the Player will move
-     public float moveSpeed = 10.0f;  // Set the default speed to 10.0f or any desired value
-    public TextMeshProUGUI speedText;  // Reference to the UI Text element
+   //I recommend 7 for the move speed, and 1.2 for the force damping
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public Vector2 forceToApply;
+    public Vector2 PlayerInput;
+    public float forceDamping;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-       // Get horizontal and vertical input from arrow keys or WASD
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        
+       // Set the "Horizontal" and "Vertical" parameters in the Animator
+        animator.SetFloat("Horizontal", PlayerInput.x);
+        animator.SetFloat("Vertical", PlayerInput.y);
 
-        // Calculate movement based on input and speed
-        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime;
+        // Set the "Speed" parameter based on the magnitude of PlayerInput
+        animator.SetFloat("Speed", PlayerInput.sqrMagnitude);
+    }
 
-        // Move the player based on the calculated movement
-        transform.Translate(movement);
-
-        if (speedText != null)
-            speedText.text = "Speed: " + moveSpeed.ToString("F2");
-        //other comments:
-        //vector3 stores the x,y,z coordinate, for example (1.0f, 0.0f, 0.0f) will make the player to move 1 unit on the x axes
-        //Time.deltaTime is making sure the rendering is smooth, and the movement speed is consistent between different devices, and it will make the animation smoother
-        //transform.Translate is responsible for the actual translate between the code and making the player move on the screen by the given axis
+     void FixedUpdate()
+    {
+        Vector2 moveForce = PlayerInput * moveSpeed;
+        moveForce += forceToApply;
+        forceToApply /= forceDamping;
+        if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
+        {
+            forceToApply = Vector2.zero;
+        }
+        rb.velocity = moveForce;
+    }
     
+
+    // Collision handling
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the character is colliding with an object tagged as "Wall"
+         // Check if the character is colliding with an object tagged as "Wall"
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+        // Debug log for testing
+        Debug.Log("Collision with wall detected");
+
+        // Stop the character from moving by setting velocity to zero
+        rb.velocity = Vector2.zero;
+        }
     }
 }
