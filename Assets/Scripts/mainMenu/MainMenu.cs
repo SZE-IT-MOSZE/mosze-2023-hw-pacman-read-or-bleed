a4aad27108b31c6aa00a67018c1889a53399bbae
Hunor1167 <kovacshunor02@gmail.com>
2023-12-08 21:11:48 +0100

    Testing map placement

--- Assets/Scripts/mainMenu/MainMenu.cs
+++ Assets/Scripts/mainMenu/MainMenu.cs
@@ -11,12 +11,6 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Level1"); 
     }
 
-    public void Test()
-    {
-        Debug.Log("Game mechanics testing map loaded");
-        SceneManager.LoadScene("Test");
-    }
-
     public void Dungeon()
     {
         Debug.Log("Dungeon testing map loaded!");
